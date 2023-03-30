efac2c1a8082dd2d2e1fd9e92171a87e233a0261
Haitian_Ji <haitianji123@gmail.com>
2023-03-30 16:12:32 +0200

    Incumplimiento del principio de Responsabilidad unica

--- src/com/kreitek/service/FileManager.java
+++ src/com/kreitek/service/FileManager.java
@@ -13,19 +13,6 @@ public class FileManager {
         return fileSystemItem.getSize();
     }
 
-    public static int calculateSize(List<FileSystemItem> files) {
-        int totalSize = 0;
-
-        for(FileSystemItem item : files) {
-            if (item instanceof File) {
-                totalSize += item.getSize();
-            } else if (item instanceof Directory) {
-                totalSize += calculateSize(item.listFiles());
-            }
-        }
-
-        return totalSize;
-    }
 
     // Aquí habría otros métodos para gestionar ficheros y directorios:
     // Crear ficheros, mover ficheros, eliminar ficheros, etc.
